af68c05ab36654f46f766957714048764c8da36e
Benjamin Lu <benlumia007@gmail.com>
2019-09-14 22:07:45 -0700

    removed host

--- setup.sh
+++ setup.sh
@@ -11,21 +11,3 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
 
     mkdir -p "sites/${domain}/public_html"
 fi
-
-get_hosts() {
-    local value=`cat ${config} | shyaml get-value sites.${domain}.host`
-    echo ${value:$@}
-}
-
-for host in `get_hosts`; do 
-    echo ${host}
-    exit 1
-    if grep -q "{{HOST}}" "config/nginx/${domain}.conf"; then
-        sed -i -e "s/{{HOST}}/${host}/g" "config/nginx/${domain}.conf"
-        rm -rf "config/nginx/${domain}.conf-e"
-    fi
-
-    if ! grep -q "${host}" /etc/hosts; then
-    echo "127.0.0.1     ${domain}.test" | sudo tee -a /etc/hosts
-    fi
-done
