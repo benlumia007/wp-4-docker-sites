eaa3f9b3e54899e0987d3e3ecf440654599d41ba
Benjamin Lu <benlumia007@gmail.com>
2019-09-14 22:26:52 -0700

    removed host

--- setup.sh
+++ setup.sh
@@ -8,6 +8,16 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
         sed -i -e "s/{{DOMAIN}}/${domain}/g" "config/nginx/${domain}.conf"
         rm -rf "config/nginx/${domain}.conf-e"
     fi
-
     mkdir -p "sites/${domain}/public_html"
+
+    if ! grep -q "${host}" /etc/hosts; then
+        echo "127.0.0.1     ${domain}.test" | sudo tee -a /etc/hosts
+    fi
+
+    cp "templates/wp-config.php" "sites/${domain}/public_html/wp-config.php"
+    sed -i -e "/DB_HOST/s/'[^']*'/'mysql'/2" "sites/${domain}/public_html/wp-config.php"
+    sed -i -e "/DB_NAME/s/'[^']*'/'${domain}'/2" "sites/${domain}/public_html/wp-config.php"
+    sed -i -e "/DB_USER/s/'[^']*'/'wordpress'/2" "sites/${domain}/public_html/wp-config.php"
+    sed -i -e "/DB_PASSWORD/s/'[^']*'/'wordpress'/2" "sites/${domain}/public_html/wp-config.php"
+    rm -rf "sites/${domain}/public_html/wp-config.php-e" 
 fi
