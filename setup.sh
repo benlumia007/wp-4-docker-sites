7559e045f77aea7b82d1c11030880a1fd8fa954e
Benjamin Lu <benlumia007@gmail.com>
2019-09-15 19:55:09 -0700

    reordered

--- setup.sh
+++ setup.sh
@@ -14,6 +14,13 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
         echo "127.0.0.1     ${domain}.test" | sudo tee -a /etc/hosts
     fi
 
+    wp core download --path=sites/${domain}/public_html
+
+    docker exec -it docker-mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
+    docker exec -it docker-mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
+    docker exec -it docker-mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
+    docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
+
     cp "templates/wp-config.php" "sites/${domain}/public_html/wp-config.php"
     sed -i -e "/DB_HOST/s/'[^']*'/'mysql'/2" "sites/${domain}/public_html/wp-config.php"
     sed -i -e "/DB_NAME/s/'[^']*'/'${domain}'/2" "sites/${domain}/public_html/wp-config.php"
@@ -21,11 +28,5 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
     sed -i -e "/DB_PASSWORD/s/'[^']*'/'wordpress'/2" "sites/${domain}/public_html/wp-config.php"
     rm -rf "sites/${domain}/public_html/wp-config.php-e" 
 
-    wp core download --path=sites/${domain}/public_html
-
-    docker exec -it docker-mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
-    docker exec -it docker-mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
-    docker exec -it docker-mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
-    docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
-    docker exec -it docker-sandbox wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --allow-root --path=/var/www/html/${domain}/public_html
+    wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --path=sites/${domain}/public_html
 fi
