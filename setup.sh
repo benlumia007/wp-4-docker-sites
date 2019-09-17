955bcf34b44d906f4c52dbd4561da425a4749c7d
Benjamin Lu <benlumia007@gmail.com>
2019-09-17 13:11:40 -0700

    updated

--- setup.sh
+++ setup.sh
@@ -28,6 +28,6 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
     sed -i -e "/DB_PASSWORD/s/'[^']*'/'wordpress'/2" "sites/${domain}/public_html/wp-config.php"
     rm -rf "sites/${domain}/public_html/wp-config.php-e"
 
-    cp "config/wordpress-compose.yml" "sites/${domain}/${domain}-compose.yml"
+    cp "config/templates/wordpress-compose.yml" "sites/${domain}/${domain}-compose.yml"
     sed -i -e "s/{{DOMAIN}}/${domain}/g" "sites{$domain}/${domain}-compose.yml"
 fi
