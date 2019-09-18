1909b9fceeaf990eaffffe1ad69653d6a30f0ea5
Benjamin Lu <benlumia007@gmail.com>
2019-09-17 22:54:13 -0700

    removed wordpress stuff and use another make create-wordpress

--- setup.sh
+++ setup.sh
@@ -14,20 +14,6 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
         echo "127.0.0.1     ${domain}.test" | sudo tee -a /etc/hosts
     fi
 
-    wp core download --path=sites/${domain}/public_html
-
-    docker exec -it docker-mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
-    docker exec -it docker-mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
-    docker exec -it docker-mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
-    docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
-
-    cp "config/templates/wp-config.php" "sites/${domain}/public_html/wp-config.php"
-    sed -i -e "/DB_HOST/s/'[^']*'/'mysql'/2" "sites/${domain}/public_html/wp-config.php"
-    sed -i -e "/DB_NAME/s/'[^']*'/'${domain}'/2" "sites/${domain}/public_html/wp-config.php"
-    sed -i -e "/DB_USER/s/'[^']*'/'wordpress'/2" "sites/${domain}/public_html/wp-config.php"
-    sed -i -e "/DB_PASSWORD/s/'[^']*'/'wordpress'/2" "sites/${domain}/public_html/wp-config.php"
-    rm -rf "sites/${domain}/public_html/wp-config.php-e"
-
     cp "config/templates/wordpress-compose.yml" "${domain}-compose.yml"
     sed -i -e "s/{{DOMAIN}}/${domain}/g" "${domain}-compose.yml"
     rm -rf "${domain}-compose.yml-e"
