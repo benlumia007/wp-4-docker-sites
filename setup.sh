4e4cac00079feb6b34fa1e5814d720a5ec0541fd
benlumia007 <benlumia007@users.noreply.github.com>
2020-05-24 17:45:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+config=".global/docker-custom.yml"
 path="${dir}/public_html"
+
+get_preprocessor() {
+    local value=`cat ${config} | shyaml get-value preprocessor 2> /dev/null`
+    echo ${value:-$@}
+}
 
 if [[ ! -f "config/nginx/${domain}.conf" ]]; then
     mkdir -p "config/nginx"
@@ -22,3 +28,35 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
         fi
     fi
 fi
+
+preprocessors=`get_preprocessor`
+
+for php in ${preprocessors//- /$'\n'}; do
+
+    if [[ ${php} == "7.2" ]]; then
+        if grep -q "7.3" config/nginx/${domain}.conf; then
+            sed -i -e "s/7.3/${php}/g" "config/nginx/${domain}.conf"
+        elif grep -q "7.4" config/nginx/${domain}.conf; then
+            sed -i -e "s/7.4/${php}/g" "config/nginx/${domain}.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/${domain}.conf"
+        fi
+    elif [[ ${php} == "7.3" ]]; then
+        if grep -q "7.2" config/nginx/${domain}.conf; then
+            sed -i -e "s/7.2/${php}/g" "config/nginx/${domain}.conf"
+        elif grep -q "7.4" config/nginx/${domain}.conf; then
+            sed -i -e "s/7.4/${php}/g" "config/nginx/${domain}.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/${domain}.conf"
+        fi
+    elif [[ ${php} == "7.4" ]]; then
+        if grep -q "7.2" config/nginx/${domain}.conf; then
+            sed -i -e "s/7.2/${php}/g" "config/nginx/${domain}.conf"
+        elif grep -q "7.3" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.3/${php}/g" "config/nginx/${domain}.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/${domain}.conf"
+        fi
+    fi
+
+done
