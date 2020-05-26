ec87def80ec0be262e8cb53cd1d899510125f3f1
benlumia007 <benlumia007@users.noreply.github.com>
2020-05-25 21:57:08 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 config=".global/docker-custom.yml"
-path="${dir}/public_html"
+site="${dir}/public_html"
+
+get_sites() {
+    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
 
 get_preprocessor() {
     local value=`cat ${config} | shyaml get-value preprocessor 2> /dev/null`
@@ -16,7 +21,7 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
         sed -i -e "s/{{DOMAIN}}/${domain}/g" "config/nginx/${domain}.conf"
         rm -rf "config/nginx/${domain}.conf-e"
     fi
-    mkdir -p "${path}"
+    mkdir -p "${site}"
 
     if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
         if ! grep -q "${domain}.test" /mnt/c/Windows/System32/drivers/etc/hosts; then
@@ -28,6 +33,36 @@ if [[ ! -f "config/nginx/${domain}.conf" ]]; then
         fi
     fi
 fi
+
+for domain in `get_sites`; do
+    get_site_provision() {
+        local value=`cat ${config} | shyaml get-value sites.${domain}.provision 2> /dev/null`
+        echo ${value:-$@}
+    }
+
+    provision=`get_site_provision`
+
+    if [[ "True" == ${provision} ]]; then
+        dir="sites/${domain}/public_html"
+        path="/srv/www/${domain}/public_html"
+
+        if [[ ! -f "${dir}/wp-config.php" ]]; then
+            docker exec -it docker-mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
+            docker exec -it docker-mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
+            docker exec -it docker-mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
+            docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
+
+            docker exec -it docker-nginx wp core download --path="${path}" --allow-root
+            docker exec -it docker-nginx wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}" --allow-root
+            docker exec -it docker-nginx wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --path=${path} --allow-root
+            docker exec -it docker-nginx wp plugin delete akismet --path=${path} --allow-root
+            docker exec -it docker-nginx wp plugin delete hello --path=${path} --allow-root
+            docker exec -it docker-nginx wp config shuffle-salts --path=${path} --allow-root
+
+            docker exec -it docker-nginx chown -R 1000:1000 ${path}
+        fi
+    fi
+done
 
 preprocessors=`get_preprocessor`
 
