fb2019614bbb220d00dbb6e60ec9d4df13b68351
benlumia007 <benlumia007@users.noreply.github.com>
2020-05-28 23:30:25 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-config=".global/docker-custom.yml"
-dir="${dir}/public_html"
-
 get_preprocessor() {
     local value=`cat ${config} | shyaml get-value preprocessor 2> /dev/null`
     echo ${value:-$@}
 }
-
-if [[ ! -f "config/nginx/${domain}.conf" ]]; then
-    mkdir -p "config/nginx"
-    cp "config/templates/nginx.conf" "config/nginx/${domain}.conf"
-
-    if grep -q "{{DOMAIN}}" "config/nginx/${domain}.conf"; then
-        sed -i -e "s/{{DOMAIN}}/${domain}/g" "config/nginx/${domain}.conf"
-        rm -rf "config/nginx/${domain}.conf-e"
-    fi
-    mkdir -p "${dir}"
-
-    if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-        if ! grep -q "${domain}.test" /mnt/c/Windows/System32/drivers/etc/hosts; then
-            echo "127.0.0.1   ${domain}.test" | sudo tee -a /mnt/c/Windows/System32/drivers/etc/hosts
-        fi
-    else
-        if ! grep -q "${domain}.test" /etc/hosts; then
-            echo "127.0.0.1   ${domain}.test" | sudo tee -a /etc/hosts
-        fi
-    fi
-fi
 
 preprocessors=`get_preprocessor`
 
