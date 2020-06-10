1cf16d0c51eef50ac31fee476cf771c3f122b752
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-10 15:49:03 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,5 @@ compose=$PWD/.global/docker-compose.yml
 path=${dir}/public_html
 
 if [[ ! -f "${path}/wp-config.php" ]]; then
-    docker-compose -f ${compose} exec mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
-    docker-compose -f ${compose} exec mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
-    docker-compose -f ${compose} exec mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'nginx'@'%' WITH GRANT OPTION;"
-    docker-compose -f ${compose} exec mysql mysql -u root -e "FLUSH PRIVILEGES;"
-    
-    
+    wp core download --path=${path}    
 fi
