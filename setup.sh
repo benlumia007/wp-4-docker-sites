87db29478ea3888b6e136ce59f06067a298980df
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-10 16:34:12 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ path=${dir}/public_html
 mkdir -p ${path}
 
 if [[ ! -f "${path}/wp-config.php" ]]; then
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"
+    mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
+    mysql mysql -u root -e "FLUSH PRIVILEGES;"
+            
     wp core download --path="${path}"
     wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
     wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
