f77a3f10bcb78adae0e83fbafbbd13cccbd4d82b
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-10 15:55:06 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,4 @@
 compose=$PWD/.global/docker-compose.yml
 path=${dir}/public_html
 
-mkdir -p ${path}
-
-if [[ ! -f "${path}/wp-config.php" ]]; then
-    wp core download --path=${path}    
-fi
+echo ${domain}
