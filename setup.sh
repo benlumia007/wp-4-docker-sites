fb1d266b86b3c47bf93bbd03d27cb364e4a142db
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-10 16:02:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,10 @@ path=${dir}/public_html
 mkdir -p ${path}
 
 if [[ ! -f "${path}/wp-config.php" ]]; then
-    wp core download --path=${path}    
+    wp core download --path="${path}"
+    wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
+    wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+    wp plugin delete akismet --path="${path}"
+    wp plugin delete hello --path="${path}"
+    wp config shuffle-salts --path="${path}"
 fi
