fffe4c6080a6401948c9fcff80b90d8e863354a6
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-15 11:11:11 -0700

    This reverts commit aca87eb0841a9b79ff71370279f76f0b95b94169.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ mkdir -p ${path}
 
 if [[ ! -f "${path}/wp-config.php" ]]; then            
     wp core download --path="${path}"
-    wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
+    wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
     wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
     wp plugin delete akismet --path="${path}"
     wp plugin delete hello --path="${path}"
