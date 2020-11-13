75177adcadad9a401c4ce577dff24fd3f9f3bc16
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-12 21:11:20 -0800

    add --activate

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ if [[ ! -f "${path}/wp-config.php" ]]; then
     noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
     noroot wp plugin delete akismet --path="${path}"
     noroot wp plugin delete hello --path="${path}"
-    noroot wp plugin install query-monitor --path="${path}"
+    noroot wp plugin install query-monitor --path="${path}" --activate
     noroot wp config shuffle-salts --path="${path}"
 fi
