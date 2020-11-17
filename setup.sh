fb3d789b797199499deb35434a438dedcb785802
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-17 15:38:55 -0800

    testing classicpress

--- setup.sh
+++ setup.sh
@@ -5,15 +5,20 @@ path=${dir}/public_html
 
 noroot mkdir -p ${path}
 
-echo ${type}
-exit 1
 
-if [[ ! -f "${path}/wp-config.php" ]]; then
-    noroot wp core download --path="${path}"
-    noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
-    noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
-    noroot wp plugin delete akismet --path="${path}"
-    noroot wp plugin delete hello --path="${path}"
-    noroot wp plugin install query-monitor --path="${path}" --activate
-    noroot wp config shuffle-salts --path="${path}"
+
+if [[ "ClassicPress" == ${type} ]]; then
+    echo ${type}
+    exit 1
+else
+    if [[ ! -f "${path}/wp-config.php" ]]; then
+        noroot wp core download --path="${path}"
+        noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
+        noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+        noroot wp plugin delete akismet --path="${path}"
+        noroot wp plugin delete hello --path="${path}"
+        noroot wp plugin install query-monitor --path="${path}" --activate
+        noroot wp config shuffle-salts --path="${path}"
+    fi
 fi
+
