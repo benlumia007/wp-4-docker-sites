67be0916381f6e9cc95710b974e8b33e8091d1db
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-17 15:41:20 -0800

    downloading classicpress

--- setup.sh
+++ setup.sh
@@ -8,8 +8,15 @@ noroot mkdir -p ${path}
 
 
 if [[ "ClassicPress" == ${type} ]]; then
-    echo ${type}
-    exit 1
+    if [[ ! -f "${path}/wp-config.php" ]]; then
+        noroot wp core download --path="${path}" https://github.com/ClassicPress/ClassicPress-release/archive/1.2.0.zip
+        noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --path="${path}"
+        noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+        noroot wp plugin delete akismet --path="${path}"
+        noroot wp plugin delete hello --path="${path}"
+        noroot wp plugin install query-monitor --path="${path}" --activate
+        noroot wp config shuffle-salts --path="${path}"
+    fi
 else
     if [[ ! -f "${path}/wp-config.php" ]]; then
         noroot wp core download --path="${path}"
