72219b7805eff7173c5a05e7099a092b5b5417c2
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-06-24 08:25:27 -0700

    add --quiet when using wp

--- setup.sh
+++ setup.sh
@@ -12,14 +12,14 @@ if [[ "none" == ${type} ]]; then
     fi
 elif [[ "ClassicPress" == ${type} ]]; then
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        noroot wp core download --path="${path}" https://www.classicpress.net/latest.zip
-        noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --path="${path}"
+        noroot wp core download --quiet --path="${path}" https://www.classicpress.net/latest.zip
+        noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --quiet --path="${path}"
         noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
         
         if [[ "${plugins}" != "none" ]]; then
           for plugin in ${plugins//- /$'\n'}; do
             if [[ "${plugin}" != "plugins" ]]; then
-              noroot wp plugin install ${plugin} --activate --path="${path}"
+              noroot wp plugin install ${plugin} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -27,7 +27,7 @@ elif [[ "ClassicPress" == ${type} ]]; then
         if [[ "${themes}" != "none" ]]; then
           for theme in ${themes//- /$'\n'}; do
             if [[ "${theme}" != "themes" ]]; then
-              noroot wp theme install ${theme} --activate --path="${path}"
+              noroot wp theme install ${theme} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -35,29 +35,29 @@ elif [[ "ClassicPress" == ${type} ]]; then
         if [[ "${constants}" != "none" ]]; then
           for const in ${constants//- /$'\n'}; do
             if [[ "${const}" != "constants" ]]; then
-              noroot wp config set --type=constant ${const} --raw true --path="${path}"
+              noroot wp config set --type=constant ${const} --raw true --quiet --path="${path}"
             fi
           done
         fi
     fi
 else
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        noroot wp core download --path="${path}"
-        noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${path}"
+        noroot wp core download --quiet --path="${path}"
+        noroot wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --quiet --path="${path}"
         noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
 
         if [[ -d "${path}/wp-content/plugins/akismet" ]]; then
-          noroot wp plugin delete akismet --path="${path}"
+          noroot wp plugin delete akismet --quiet --path="${path}"
         fi 
 
         if [[ -f "${path}/wp-content/plugins/hello.php" ]]; then
-          noroot wp plugin delete hello --path="${path}"
+          noroot wp plugin delete hello --quiet --path="${path}"
         fi 
 
         if [[ "${plugins}" != "none" ]]; then
           for plugin in ${plugins//- /$'\n'}; do
             if [[ "${plugin}" != "plugins" ]]; then
-              noroot wp plugin install ${plugin} --activate --path="${path}"
+              noroot wp plugin install ${plugin} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -65,7 +65,7 @@ else
         if [[ "${themes}" != "none" ]]; then
           for theme in ${themes//- /$'\n'}; do
             if [[ "${theme}" != "themes" ]]; then
-              noroot wp theme install ${theme} --activate --path="${path}"
+              noroot wp theme install ${theme} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -73,7 +73,7 @@ else
         if [[ "${constants}" != "none" ]]; then
           for const in ${constants//- /$'\n'}; do
             if [[ "${const}" != "constants" ]]; then
-              noroot wp config set --type=constant ${const} --raw true --path="${path}"
+              noroot wp config set --type=constant ${const} --raw true --quiet --path="${path}"
             fi
           done
         fi
